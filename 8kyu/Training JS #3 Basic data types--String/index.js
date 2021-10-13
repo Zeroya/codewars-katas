a64341232a36b4b767a92118bbf1f7a7cc921bfa
Zeroya <36275430+Zeroya@users.noreply.github.com>
2021-10-13 10:55:32 +0300

    Update index.js

--- 8kyu/Training JS #3 Basic data types--String/index.js
+++ 8kyu/Training JS #3 Basic data types--String/index.js
@@ -1,27 +1,27 @@
-var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
-function equal1(){
-  var a=v1   
-  var b=v1   
-  return a+b;
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  //select some variable to combine "Dad"
+  return d1+a2+d2;
 }
-//Please refer to the example above to complete the following functions
-function equal2(){
-  var a=v6   //set number value to a
-  var b=v3   //set number value to b
-  return a-b;
+function Bee(){
+  //select some variable to combine "Bee"
+  return b1+e2+e2;
 }
-function equal3(){
-  var a=v1   //set number value to a
-  var b=v5   //set number value to b
-  return a*b;
+function banana(){
+  //select some variable to combine "banana"
+  return b2+a2+n2+a2+n2+a2;
 }
-function equal4(){
-  var a=v4   //set number value to a
-  var b=v5   //set number value to b
-  return a/b;
+
+//answer some questions if you finished works above
+function answer1(){
+  //the answer should be "yes" or "no"
+  return "no";
 }
-function equal5(){
-  var a=v2  //set number value to a
-  var b=v6  //set number value to b
-  return a%b;
+function answer2(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer3(){
+  //the answer should be "yes" or "no"
+  return "yes";
 }
