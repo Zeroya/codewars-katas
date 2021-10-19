fba64cfbe9e42381765669b0ae7716b85bdcc8e0
Zeroya <36275430+Zeroya@users.noreply.github.com>
2021-10-19 22:57:39 +0300

    Update index.js

--- 8kyu/Training JS #14 Methods of Number object--toString() and toLocaleString()/index.js
+++ 8kyu/Training JS #14 Methods of Number object--toString() and toLocaleString()/index.js
@@ -1,27 +1,10 @@
-var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
-function equal1(){
-  var a=v1   
-  var b=v1   
-  return a+b;
-}
-//Please refer to the example above to complete the following functions
-function equal2(){
-  var a=v6   //set number value to a
-  var b=v3   //set number value to b
-  return a-b;
-}
-function equal3(){
-  var a=v1   //set number value to a
-  var b=v5   //set number value to b
-  return a*b;
-}
-function equal4(){
-  var a=v4   //set number value to a
-  var b=v5   //set number value to b
-  return a/b;
-}
-function equal5(){
-  var a=v2  //set number value to a
-  var b=v6  //set number value to b
-  return a%b;
+function colorOf(r,g,b){
+  //coding here
+  var r1 = r.toString(16);
+  var g1 = g.toString(16);
+  var b1 = b.toString(16);
+  r1 = r1.length == 1 ? '0' + r1 : r1;
+  g1 = g1.length == 1 ? '0' + g1 : g1;
+  b1 = b1.length == 1 ? '0' + b1 : b1;
+  return "#" + r1 + g1 + b1
 }
